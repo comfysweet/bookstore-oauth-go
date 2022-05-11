500d5d5b62ed4d694e5a9651662c1af8371b0cf6
comfysweet <kszemsk@gmail.com>
2022-05-11 16:22:29 +0300

    bookstore-oauth

---
+++ oauth/oauth_test.go
@@ -0,0 +1,49 @@
+package oauth
+
+import (
+	"fmt"
+	"github.com/mercadolibre/golang-restclient/rest"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("start tests")
+
+	rest.StartMockupServer()
+
+	os.Exit(m.Run())
+}
+
+func TestIsPublicNilRequest(t *testing.T) {
+	assert.True(t, IsPublic(nil))
+}
+
+func TestIsPublicNoError(t *testing.T) {
+	request := http.Request{
+		Header: make(http.Header),
+	}
+	assert.False(t, IsPublic(&request))
+
+	request.Header.Add("X-Public", "true")
+	assert.True(t, IsPublic(&request))
+}
+
+func TestGetAccessTokenInvalid(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		HTTPMethod:   http.MethodGet,
+		URL:          "http://localhost:8080/oauth/access_token/ABC123",
+		ReqBody:      ``,
+		RespHTTPCode: -1,
+		RespBody:     `{}`,
+	})
+
+	accessToken, err := getAccessToken("ABC123")
+	assert.Nil(t, accessToken)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "invalid rest client response when trying to get access token", err.Message)
+}
